1a2e824f850fa1d6a9120bef62aa306db042b1e5
Sim-km <simkm0824@naver.com>
2024-08-22 00:01:32 +0900

    refactor: 클래스 내 필드 값 private으로 변경

--- s3_handler.py
+++ s3_handler.py
@@ -7,15 +7,15 @@ from image_supporter import ImageFormatter
 
 class S3ImageCompactSupporter:
     def __init__(self):
-        self.s3_client = boto3.client('s3')
-        self.image_threshold = 1000000  # 1MB
-        self.image_quality = 80
-        self.default_image_type = 'jpeg'
-        self.image_format_supporter = ImageFormatter(self.default_image_type)
+        self.__s3_client = boto3.client('s3')
+        self.__image_threshold = 1000000  # 1MB
+        self.__image_quality = 80
+        self.__default_image_type = 'jpeg'
+        self.__image_format_supporter = ImageFormatter(self.__default_image_type)
 
-        self.image_diff_threshold = 1000
+        self.__image_diff_threshold = 1000
 
-        self.image_acl = 'public-read'
+        self.__image_acl = 'public-read'
 
     def process_s3_metadata(self, event_records):
         for meta_data in event_records:
@@ -27,28 +27,28 @@ class S3ImageCompactSupporter:
         key = urllib.parse.unquote_plus(s3_meta_data['object']['key'], encoding='utf-8')
         size = int(s3_meta_data['object']['size'])
 
-        if size > self.image_threshold:
+        if size > self.__image_threshold:
             image_path = self._image_download(bucket_name, key)
-            transformed_image_path = self.image_format_supporter.compact_image(image_path, self.image_quality)
+            transformed_image_path = self.__image_format_supporter.compact_image(image_path, self.__image_quality)
             self._upload_image(bucket_name, key, transformed_image_path)
 
     def _image_download(self, bucket_name, key):
         print('Downloading image from s3')
         image_download_path = '/tmp/' + key.split('/')[-1]
-        self.s3_client.download_file(bucket_name, key, image_download_path)
+        self.__s3_client.download_file(bucket_name, key, image_download_path)
 
-        transformed_image_path = self.image_format_supporter.transform_image(image_download_path)
+        transformed_image_path = self.__image_format_supporter.transform_image(image_download_path)
 
         print('Image downloaded to ' + transformed_image_path)
         return transformed_image_path
 
     def _upload_image(self, bucket_name, key, image_path):
-        content_type = 'image/' + self.default_image_type
+        content_type = 'image/' + self.__default_image_type
 
-        old_image_size = self.s3_client.head_object(Bucket=bucket_name, Key=key)['ContentLength']
+        old_image_size = self.__s3_client.head_object(Bucket=bucket_name, Key=key)['ContentLength']
         new_image_size = os.path.getsize(image_path)
-        if abs(old_image_size - new_image_size) < self.image_diff_threshold:
+        if abs(old_image_size - new_image_size) < self.__image_diff_threshold:
             return
 
-        self.s3_client.put_object(Bucket=bucket_name, Key=key, Body=open(image_path, 'rb'), ContentType=content_type)
+        self.__s3_client.put_object(Bucket=bucket_name, Key=key, Body=open(image_path, 'rb'), ContentType=content_type)
         os.remove(image_path)
